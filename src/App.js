546fe40cd79af5e640787eff6a5d2ff9b2bfd26e
ahza64 <justinmichaelpettit@gmail.com>
2022-04-11 15:30:46 -0700

    modal added without handler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Todo from "./components/Todo"
+import Modal  from "./components/Modal"
+import Backdrop from "./components/Backdrop"
 
 function App() {
   return (
@@ -6,6 +8,8 @@ function App() {
       <h1>Badass Todos</h1>
       <Todo text="digin it breakin it" />
       <Todo text="piles and piles" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
