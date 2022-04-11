a019dbcf5f9ca50b4d922aec089c04e6e3d902cb
ahza64 <justinmichaelpettit@gmail.com>
2022-04-11 15:59:23 -0700

    finish modal handlers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Todo from "./components/Todo"
-import Modal  from "./components/Modal"
-import Backdrop from "./components/Backdrop"
 
 function App() {
   return (
@@ -8,8 +6,6 @@ function App() {
       <h1>Badass Todos</h1>
       <Todo text="digin it breakin it" />
       <Todo text="piles and piles" />
-      <Modal />
-      <Backdrop />
     </div>
   );
 }
